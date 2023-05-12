58f9c9a689dd5859c981c44f6f4ffc965795b02d
Volkan Güney <90406090+gnyvlkn@users.noreply.github.com>
2023-05-11 23:55:38 -0400

    Update Covid_Data_Exploration_SQL.sql

--- Covid_Data_Exploration_SQL.sql
+++ Covid_Data_Exploration_SQL.sql
@@ -1,3 +1,11 @@
+/*
+Covid 19 Data Exploration 
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+*/
+
+
 SELECT *
 FROM [PortfolioProject].[dbo].[CovidDeaths]
 order by 3,4
